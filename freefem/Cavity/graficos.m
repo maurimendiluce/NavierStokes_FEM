260bb81d9ec02071bfe2abc641633c75e4a254cd
Mauricio Mendiluce <mauricio.mendiluce@gmail.com>
2024-05-29 17:44:23 -0300

    actualizo

--- freefem/Cavity/graficos.m
+++ freefem/Cavity/graficos.m
@@ -1,29 +1,32 @@
 addpath('ffmatlib');
 
-%mesh
-[p,b,t,nv,nbe,nt,labels]=ffreadmesh('mesh_square.msh');
-ffpdeplot(p,b,t,'Mesh','on','Boundary','on');
-nodos=p(1:2,:)';
-elem=t(1:4,:)';
+clear all;
 
-Vh=ffreaddata('espacio_Vh.txt');
+addpath('ffmatlib');
+[p,b,t]=ffreadmesh('mesh_square.msh');
+[vhseq]=ffreaddata('espacio_Vh.txt');
+[qhseq]=ffreaddata('espacio_Qh.txt');
+[u1,u2]=ffreaddata('solucion.txt');
+[dp]=ffreaddata('presion.txt');
 
-ph=ffreaddata('presion.txt');
-
-%figure;
-%ffpdeplot(p,b,t,'VhSeq',Vh,'XYData',ph,'Mesh','on','Boundary','on','XLim',[0 1],'YLim',[0 1]);
-
-figure;     
-pdesurf(nodos',elem',ph)%, shading interp
-
-[u1,u2]=ffreaddata('velocidad.txt');
+%%Mesh
+figure();
+ffpdeplot(p,b,t, ...
+          'Mesh','on', ...
+          'Boundary','on', ...
+          'Title','Mesh');
 
 
-%x=nodos(:,1);
-%y=nodos(:,2);
+%%%%%%% 3D Surface
+figure();
+ffpdeplot(p,b,t, ...
+          'XYData',dp,'VhSeq',qhseq,'ZStyle','continuous', ...
+          'Title','Solución ph');
 
-figure;
-ffpdeplot(p,b,t,'VhSeq',Vh,'XYData',ph,'Mesh','off','Boundary','on', ...
-          'XLim',[0 1],'YLim',[0 1],'Contour','on','CColor','b', ...
-          'XYStyle','off', 'CGridParam',[150, 150],'ColorBar','off', ...
-         'FlowData',[u1,u2],'FGridParam',[24, 24]);
+
+figure();
+ffpdeplot(p,b,t, ...
+          'FlowData',[u1,u2],'VhSeq',vhseq,'FGridParam',[26,30],'Boundary','on', ...
+          'Title','Solución uh');
+
+axis tight;
